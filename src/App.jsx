6d78466a8b2d8fe54b9b1bf7af35b637b0072dab
COValhalla <josephvellella@proton.me>
2022-10-28 15:38:33 -0600

    Initial boilerplate.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navigation from './assets/Navbar';
+
+function App() {
+  return (
+    <div className="">
+      <Navigation />
+      <h1>React Bootstrap</h1>
+    </div>
+  );
+}
+
+export default App;
