1a5366249bf513a53b8e32d318b72e51bf24b27e
COValhalla <josephvellella@proton.me>
2022-10-31 11:53:19 -0600

    Individual blog view working.

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Blog from './assets/components/Blog';
 import Navigation from './assets/partials/Navbar';
+import BlogView from './assets/components/BlogView';
 
 const RouteSwitch = () => {
   return (
@@ -10,6 +11,7 @@ const RouteSwitch = () => {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/blog" element={<Blog />} />
+        <Route path="/blogs/:id" element={<BlogView />} />
       </Routes>
     </BrowserRouter>
   );
